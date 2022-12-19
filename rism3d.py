Change residual calculation from 'mean' to 'max'. This partially fix sfe dependence from box size for non-charged solutes.

rism3d.py
@@ -34,7 +34,8 @@ class Rism3D:
         while True:
             self._closure()
             self._oz()
-            e = np.mean(np.abs(self._gamma - gamma_old))
+#            e = np.mean(np.abs(self._gamma - gamma_old))
+            e = np.max(np.abs(self._gamma - gamma_old))
             self._gamma = mix * self._gamma + (1 - mix) * gamma_old
             step += 1
             gamma_old = self._gamma.copy()