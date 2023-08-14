c23dd266e862a7cd16fd837beb5b2808d4cc7701
kse <kse@laptop.my>
2023-08-14 16:16:31 +0300

    "calculate_inverse_fourier_bessel()" function input changed from 1-D array to multidimensional array

--- fourier.py
+++ fourier.py
@@ -5,23 +5,25 @@ from scipy import interpolate
 
 def calculate_inverse_fourier_bessel(data, r_delta, k_delta):
     """Calculate inverse Fourier-Bessel transform. 
-    The input function should be tabulated on uniformly spaced grid. 
+    The input functions should be tabulated on uniformly spaced grid. 
+    In case of array of functions transformation is performed over 
+    last dimension of data array. 
 
     Parameters:
-        data : 1-D ndarray of input values.
+        data : array of input values.
         r_delta : the grid spacing in the direct space.
         k_delta : the grid spacing in the inverse space.
     Returns:
-        1-D array of values of transformed function.
+        array of values of transformed functions.
     Raises:
         ValueError : If any value of input data 
                      is non-finite (inf's or NaN's) 
     """
     if np.any(~np.isfinite(data)):
         raise ValueError("Input data consists infinities or NaN's")
-    index = np.arange(len(data))
+    index = np.arange(data.shape[-1])
     f = np.zeros_like(data)
-    f[0] = 4 * np.pi * k_delta**3 * np.sum(data * index**2)
-    f[1:] = (k_delta**2 / (index[1:] * r_delta) 
-             * fftpack.dst(data[1:] * index[1:], type=1))
+    f[..., 0] = 4 * np.pi * k_delta**3 * np.sum(data * index**2, axis=-1)
+    f[..., 1:] = (k_delta**2 / (index[1:] * r_delta) 
+             * fftpack.dst(data[..., 1:] * index[1:], type=1))
     return f
