Fix the mistake of calculation of 1-D k-grid values from AmberTools data. Add renormalization (division on 2 * pi) of k-grid in "read_solvent()" function

rism3d.py
@@ -110,7 +110,7 @@ class Rism3D:
                             - self._h_l[e <= 0]) 
         
     def _calculate_susceptibility(self):
-        k_1d = (self._solvent["k_grid"]) / 2 / np.pi
+        k_1d = self._solvent["k_grid"]
         npoints = len(self._solvent["k_grid"])
         chi_1d = self._solvent["chi"]
         k_3d = np.linalg.norm(self._k_grid, axis=0)

utils.py
@@ -72,7 +72,7 @@ def read_solvent(xvv_file):
     data["chi"] = xvv["XVV"].reshape((nsites, nsites, npoints), order="C")
     data["chi"] = np.transpose(data["chi"], axes=(1, 0, 2))
     r_delta = xvv["THERMO"][4]
-    k_delta = np.pi / (npoints + 1) / r_delta
+    k_delta = 1 / (2 * npoints * r_delta)
     data["k_grid"] = np.arange(npoints) * k_delta
     return data
     