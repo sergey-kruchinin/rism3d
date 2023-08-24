f2f1da93168805331731d9436f9a6019f86b8901
kse <kse@laptop.my>
2023-08-24 14:01:29 +0300

    Add Perkyns electrostatic treatment method (function "_get_renormalized_potential()"). Add "pse3" closure

--- rism3d.py
+++ rism3d.py
@@ -7,13 +7,15 @@ import itertools
 import constants
 import mdiis
 import exceptions
+import potentials
+import fourier
 
 
 class Rism3D:
     def __init__(self, solute, solvent, box, options):
         self._options = copy.deepcopy(options)
         self._beta = 1 / constants.k_Boltzmann / self._options["temperature"] 
-        closures = {"hnc": self._hnc, "kh": self._kh}
+        closures = {"hnc": self._hnc, "kh": self._kh, "pse3": self._use_pse3}
         self._closure = closures[self._options["closure"]]
         solvers = {"picard": self._picard_solver, "mdiis": self._mdiis_solver}
         self._solver = solvers[self._options["solver"]]
@@ -26,6 +28,7 @@ class Rism3D:
         self._v_s = self._calculate_short_potential()
         self._h_l = self._calculate_long_tcf()
         self._v_l = self._calculate_long_potential()
+        self._theta = self._get_renormalized_potential()
         self._c_s = np.zeros_like(self._v_s)
         self._gamma = np.zeros_like(self._v_s)
         
@@ -40,6 +43,7 @@ class Rism3D:
         while True:
             self._closure()
             self._oz()
+            self._gamma -= self._theta 
             e = np.max(np.abs(self._gamma - gamma_old))
             self._gamma = mix * self._gamma + (1 - mix) * gamma_old
             step += 1
@@ -64,6 +68,7 @@ class Rism3D:
         while True:
             self._closure()
             self._oz()
+            self._gamma -= self._theta 
             residual = self._gamma - gamma_old
             self._gamma = m.optimize(gamma_old, residual)
             e = np.max(np.abs(residual))
@@ -79,7 +84,7 @@ class Rism3D:
                 break
 
     def h(self):
-        h = self._c_s + self._gamma + self._h_l
+        h = self._c_s + self._gamma
         return h
 
     def c(self):
@@ -94,20 +99,24 @@ class Rism3D:
         self._gamma = self._inverse_fourier(gamma_ft)
 
     def _hnc(self):
-        self._c_s = (np.exp(-self._v_s 
-                            + self._h_l 
-                            + self._gamma) 
-                     - 1 
-                     - self._gamma 
-                     - self._h_l)
+        self._c_s = (np.exp(-self._v_s + self._gamma) 
+                     - 1 - self._gamma)
 
     def _kh(self):
-        e = -self._v_s + self._h_l + self._gamma
+        e = -self._v_s + self._gamma
         self._c_s[e > 0] = -self._v_s[e > 0]
         self._c_s[e <= 0] = (np.exp(e[e <= 0]) 
                             - 1 
-                            - self._gamma[e <= 0] 
-                            - self._h_l[e <= 0]) 
+                            - self._gamma[e <= 0]) 
+
+    def _use_pse3(self):
+        e = -self._v_s + self._gamma
+        self._c_s[e > 0] = (-self._v_s[e > 0] 
+                            + (1.0 / 2.0) * e[e > 0]**2 
+                            + (1.0 / 6.0) * e[e > 0]**3)
+        self._c_s[e <= 0] = (np.exp(e[e <= 0]) 
+                            - 1 
+                            - self._gamma[e <= 0]) 
         
     def _calculate_susceptibility(self):
         k_1d = self._solvent["k_grid"]
@@ -271,3 +280,32 @@ class Rism3D:
         grids[-1] = fft.rfftfreq(*points_and_steps[-1])
         k_grid = np.stack(np.meshgrid(*grids, indexing="ij"))
         return k_grid
+
+    def _get_renormalized_potential(self):
+        solvent_charges = self._solvent["charge"]
+        chi = self._solvent["chi"]
+        theta_site_ft = np.einsum("i,ijk->jk", solvent_charges, chi)
+        k_1d = self._solvent["k_grid"]
+        phi_ft = (self._beta 
+                  * potentials.make_inverse_long_coulomb_potential(k_1d, 1, 1))
+        theta_site_ft = theta_site_ft * phi_ft
+        k_delta = k_1d[1] - k_1d[0]
+        number_of_points = len(k_1d)
+        r_delta = 1.0 / (2 * number_of_points * k_delta)
+        theta_site = fourier.calculate_inverse_fourier_bessel(theta_site_ft, 
+                                                              r_delta, 
+                                                              k_delta)
+        r_1d = np.arange(number_of_points) * r_delta
+        f = interpolate.interp1d(r_1d, 
+                                 theta_site, 
+                                 kind="cubic", 
+                                 fill_value="extrapolate")
+        theta_shape = (self._solvent["multy"].shape 
+                       + self._r_grid.shape[1:])
+        theta = np.zeros(theta_shape)
+        for q, xyz in zip(self._solute["charge"], self._solute["xyz"]):
+            site_position = np.expand_dims(xyz, axis=(1, 2, 3))
+            distances = np.linalg.norm(self._r_grid - site_position, axis=0)
+            theta_site_interpolated = f(distances)
+            theta = theta + q * theta_site_interpolated
+        return theta 
