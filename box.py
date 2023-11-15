82fe11f306291ca7126ce59885ff4ef67711a80b
kse <kse@laptop.my>
2023-11-15 17:37:14 +0300

    Rewrite default constructor of the "Box" class for use central point instead of use the "Solute" instance. Add two additional constructors, one for use with the "Solute" instance and another to make the "Box" from the "openDX" instance.

--- box.py
+++ box.py
@@ -3,31 +3,71 @@ from scipy import fft
 
 
 class Box:
-    def __init__(self, solute, deltas, buffer):
-        """Create box around solute."""
-        min_bounds = np.min(solute.coordinates, axis=0) - buffer
-        max_bounds = np.max(solute.coordinates, axis=0) + buffer
-        box_dimensions = max_bounds - min_bounds
+    def __init__(self, origin, deltas, buffer):
+        """Constructor.
+        Create rectangular box around origin point.
+
+        Parameters:
+            origin : 1d ndarray. Cartesian coordinates of the box 
+                     center.
+            deltas : 1d ndarray. Grid spacings for every of three 
+                     dimensions.
+            buffer : scalar of 1d ndarray. Space between origin and box 
+                     faces for every dimension. When only one value is 
+                     supplied it used as common value and resulting Box
+                     will be cubic.
+        Raises:
+            ValueError : When at least one value of buffer is 
+                         negative.
+        """
+        if buffer.any() < 0:
+            raise ValueError("Box buffer value(s) " 
+                             + str(buffer) 
+                             + " cannot be negative")
+        box_dimensions = buffer * 2
         npoints = np.ceil(box_dimensions / deltas).astype(int) + 1
         half_widths = (npoints - 1) * deltas / 2
-        box_center = solute.get_center()
-        min_bounds = box_center - half_widths
-        max_bounds = box_center + half_widths
+        min_bounds = origin - half_widths
+        max_bounds = origin + half_widths
         box = [(m, M, p) for m, M, p in zip(min_bounds, max_bounds, npoints)]
         self._r_grid = self._get_r_grid(box)
         self._k_grid = self._get_k_grid(npoints, deltas)
         self._delta = np.eye(3) * deltas
         self._cell_volume = np.prod(deltas)
 
-#    @classmethod
-#    def from_dx(cls, dx):
-#        deltas = np.diagonal(dx.delta)
-#        min_border = dx.origin
-#        npoints = dx.size
-#        max_border = (npoints - 1) * deltas + min_border
-#        box = [(l, h, p) for l, h, p in zip(min_border, max_border, points)]
-#        return box
-#
+    @classmethod
+    def around_solute(cls, solute, deltas, buffer):
+        """Constructor.
+        Create rectangular box around solute.
+
+        Parameters:
+            origin : Solute instance. 
+            deltas : 1d ndarray. Grid spacings for every of three 
+                     dimensions.
+            buffer : scalar of 1d ndarray. Space between soluteand box 
+                     faces for every dimension. When only one value is 
+                     supplied it used as common value.
+        """
+        origin = solute.get_center()
+        half_box_widths = np.max(solute.coordinates, axis=0) + buffer - origin
+        return cls(origin, deltas, half_box_widths)
+
+    @classmethod
+    def create_from_dx(cls, dx):
+        """Constructor.
+        Create box from the openDX object.
+
+        Parameters:
+            dx : openDX object.
+        """
+        deltas = np.diagonal(dx.delta)
+        min_border = dx.origin
+        npoints = dx.size
+        max_border = (npoints - 1) * deltas + min_border
+        origin = (max_border + min_border) / 2
+        buffer = max_border - origin
+        return cls(origin, deltas, buffer)
+
     @property
     def r_grid(self):
         return self._r_grid
