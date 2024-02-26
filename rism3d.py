cae6b718d6d6cc85b15cecbe4faaff4e86b705c6
kse <kse@laptop.my>
2024-02-26 17:25:29 +0300

    Delete Picard and MDIIS solvers code from Rism3D class.

--- rism3d.py
+++ rism3d.py
@@ -22,9 +22,6 @@ class Rism3D:
                     "kh": self._use_kh, 
                     "pse3": self._use_pse3}
         self._use_closure = closures[self._parameters["closure"]]
-        solvers = {"picard": self._use_picard_solver, 
-                   "mdiis": self._use_mdiis_solver}
-        self._use_solver = solvers[self._parameters["solver"]]
         self._chi = _get_susceptibility(self._solvent, self._box)
         self._v_s = (potentials.get_lj(self._solute, 
                                        self._solvent, 
@@ -46,10 +43,7 @@ class Rism3D:
                                                   self._beta)
         self._gamma = np.zeros_like(self._v_s)
         self._c_s = np.zeros_like(self._v_s)
-        self._step = 0
-        self._set_solver()
 
-# temporary getters
     @property
     def gamma(self):
         return self._gamma
@@ -67,10 +61,6 @@ class Rism3D:
         return self._use_oz
 
     @property
-    def theta(self):
-        return self._theta
-        
-    @property
     def parameters(self):
         return self._parameters
 
@@ -82,112 +72,13 @@ class Rism3D:
     def beta(self):
         return self._beta
 
-#    def solve(self):
-#        self._use_solver()
-
-#    def solve(self):
-#        while True:
-#            self.iterate()
-#            e = np.max(np.abs(self._gamma - self._gamma_old))
-#            print("{0:<6d}{1:18.8e}".format(self._step, e))
-#            if step >= self._parameters["nsteps"]:
-#            raise exceptions.Rism3DMaxStepError("The maximum number of steps has been reached", step, e)
-#        if np.isnan(e) or np.isinf(e):
-#            raise exceptions.Rism3DConvergenceError("The solution has been diverged", step)
-#        if e < self._parameters["accuracy"]:
-#            break
-
     def get_h(self):
-#        c_s = self._use_closure()
         h = self._c_s + self._gamma
         return h
 
     def get_c(self):
-#        c_s = self._use_closure()
         c = self._c_s - self.beta * self._v_l
         return c
-
-    def _set_solver(self):
-        setters = {"picard": self._set_picard_solver, 
-                   "mdiis": self._use_mdiis_solver}
-        setters[self._parameters["solver"]]
-
-    def _set_picard_solver(self):
-        self._mix = self._parameters["mix"]
-        self._gamma = np.zeros_like(self._v_s)
-        self._gamma_old = np.zeros_like(self._v_s)
-        print("{0:<6s}{1:>18s}".format("step", "accuracy"))
-        self.iterate = self._step_picard
-        
-#    def _step_picard(self):
-#        c_s = self._use_closure()
-#        self._use_oz(c_s)
-#        self._gamma -= self._theta 
-#        e = np.max(np.abs(self._gamma - self._gamma_old))
-#        self._gamma = (self._mix * self._gamma 
-#                       + (1 - self._mix) * self._gamma_old)
-#        self._step += 1
-#        self._gamma_old = self._gamma.copy()
-#        print("{0:<6d}{1:18.8e}".format(step, e))
-#        if step >= self._parameters["nsteps"]:
-#            raise exceptions.Rism3DMaxStepError("The maximum number of steps has been reached", step, e)
-#        if np.isnan(e) or np.isinf(e):
-#            raise exceptions.Rism3DConvergenceError("The solution has been diverged", step)
-#        if e < self._parameters["accuracy"]:
-#            break
-        
-    def _use_picard_solver(self):
-        mix = self._parameters["mix"]
-        gamma_old = self._gamma.copy()
-        step = 0
-        print("{0:<6s}{1:>18s}".format("step", "accuracy"))
-        while True:
-            c_s = self._use_closure()
-            self._use_oz(c_s)
-            self._gamma -= self._theta 
-            e = np.max(np.abs(self._gamma - gamma_old))
-            self._gamma = mix * self._gamma + (1 - mix) * gamma_old
-            step += 1
-            gamma_old = self._gamma.copy()
-            print("{0:<6d}{1:18.8e}".format(step, e))
-            if step >= self._parameters["nsteps"]:
-                raise exceptions.Rism3DMaxStepError("The maximum number of steps has been reached", step, e)
-            if np.isnan(e) or np.isinf(e):
-                raise exceptions.Rism3DConvergenceError("The solution has been diverged", step)
-            if e < self._parameters["accuracy"]:
-                break
-                
-    def _use_mdiis_solver(self):
-        m = mdiis.MDIIS(self._parameters["mdiis_vectors"],
-                        self._parameters["mdiis_mix"],
-                        self._parameters["mdiis_max_residue"]
-                        )
-        gamma_old = self._gamma.copy()
-        step = 0
-        print("{0:<6s}{1:>18s}{2:>7s}".format("step", "accuracy", "MDIIS"))
-        while True:
-            c_s = self._use_closure()
-            self._use_oz(c_s)
-            self._gamma -= self._theta 
-            residual = self._gamma - gamma_old
-            self._gamma = m.optimize(gamma_old, residual)
-            e = np.max(np.abs(residual))
-            step += 1
-            gamma_old = self._gamma.copy()
-            print(f"{step:<6d}{e:18.8e}{m.size():>7d}")
-            if step >= self._parameters["nsteps"]:
-                raise exceptions.Rism3DMaxStepError("The maximum number of steps has been reached", step, e)
-            if np.isnan(e) or np.isinf(e):
-                raise exceptions.Rism3DConvergenceError("The solution has been diverged", step)
-            if e < self._parameters["accuracy"]:
-                break
-
-#    def _use_oz(self, c_s):
-#        c_s_ft = _get_fourier_transform(c_s, self._box)
-#        gamma_ft = np.sum(self._chi 
-#                          * np.expand_dims(c_s_ft, axis=1),
-#                          axis=0) - c_s_ft
-#        self._gamma = _get_inverse_fourier_transform(gamma_ft, self._box)
 
     def _use_oz(self):
         c_s_ft = _get_fourier_transform(self._c_s, self._box)
